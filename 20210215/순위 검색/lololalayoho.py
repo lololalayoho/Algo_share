c33ba1983617e957a9c086097a302d0ef31c2629
lololalayoho <3chlwlsdn@hanmail.net>
2021-02-22 16:43:39 +0900

    순위 검색 해결

--- 20210215/순위 검색/lololalayoho.py
+++ 20210215/순위 검색/lololalayoho.py
@@ -1,5 +1,4 @@
 # https://programmers.co.kr/learn/courses/30/lessons/72412
-# 시간 초과
 infos = []
 values = []
 visit = [0 for i in range(4)]
@@ -14,16 +13,19 @@ def make_order(word, start, cnt, totalcnt):
                 sentence = sentence + "-"
         if sentence not in infos:
             infos.append(sentence)
-            value = [word[4]]
+            value = [int(word[4])]
             values.append(value)
         else:
-            values[infos.index(sentence)].append(word[4])
+            info_index = infos.index(sentence)
+            values[info_index].append(int(word[4]))
     else:
         for i in range(start, 4):
             if visit[i] == 0:
                 visit[i] = 1
                 make_order(word, i + 1, cnt + 1, totalcnt)
                 visit[i] = 0
+
+
 def solution(info, query):
     answer = []
     for i in info:
@@ -35,8 +37,13 @@ def solution(info, query):
         word.append(i.split(' ')[4])
         for j in range(5):
             make_order(word, 0, 0, j)
+    kakao = []
+    for index, i in enumerate(infos):
+        kakao.append((i, values[index]))
+    kakaoinfo = dict(kakao)
+    for ka in kakaoinfo.values():
+        ka.sort()
     for i in query:
-        cnt = 0
         sentence = ""
         word = []
         word.append(i.split(' ')[0])
@@ -47,8 +54,17 @@ def solution(info, query):
         for j in range(4):
             sentence = sentence + word[j]
         if sentence in infos:
-            for j in values[infos.index(sentence)]:
-                if int(word[4]) <= int(j):
-                    cnt = cnt + 1
-        answer.append(cnt)
+            data = kakaoinfo[sentence]
+            if len(data) > 0:
+                start, end = 0, len(data)
+                while start != end and start != len(data):
+                    if data[(start + end) // 2] >= int(word[4]):
+                        end = (start + end) // 2
+                    else:
+                        start = (start + end) // 2 + 1
+                answer.append(len(data) - start)
+            else:
+                answer.append(0)
+        else:
+            answer.append(0)
     return answer
